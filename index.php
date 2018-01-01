86b590c85599a987f205037d9d8abecb66a258c2
Dan Levy <dan@danlevy.net>
2018-01-01 16:45:44 -0700

    fix stupid tabs

--- index.php
+++ index.php
@@ -8,8 +8,10 @@ Author: Dan Levy
 Author URI: https://github.com/justsml
 License: MIT
 */
+$actions = array();
 
 function registerAjax($funcName, $requireLogin = true) {
+  $actions[] = $funcName;
   if ($requireLogin) {
     add_action('wp_ajax_nopriv_' . $funcName, '' . $funcName);
   }
@@ -21,33 +23,33 @@ function registerAjax($funcName, $requireLogin = true) {
  */
 registerAjax('get_customer', true);
 function get_customer() {
-	global $wpdb;
-	$email = $_POST['email'];
-	$results = search_customer($email);
-	wp_send_json_success($results);
+  global $wpdb;
+  $email = $_POST['email'];
+  $results = search_customer($email);
+  wp_send_json_success($results);
 }
 
 function search_customer($email) {
-	global $wpdb;
-	$query = 'SELECT wpcx_customers.salesrep,
-		wpcx_customers.display_name as cust_display_name,
-		wpcx_customers.email as cust_email,
-		wpcx_customers.primaryphone as cust_primaryphone,
-		wpcx_customers.address as cust_address,
-		wpcx_customers.city as cust_city,
-		wpcx_customers.state as cust_state,
-		wpcx_customers.zip as cust_zip,
-		wpcx_users.display_name as rep_display_name,
-		wpcx_users.email as rep_email
-	FROM wpcx_customers
-		INNER JOIN wpcx_users
-			ON wpcx_customers.location = wpcx_users.location
-		INNER JOIN wpcx_customers
-			ON wpcx_users.location = wpcx_customers.location
-	WHERE wpcx_customers.email=%s';
+  global $wpdb;
+  $query = 'SELECT wpcx_customers.salesrep,
+    wpcx_customers.display_name as cust_display_name,
+    wpcx_customers.email as cust_email,
+    wpcx_customers.primaryphone as cust_primaryphone,
+    wpcx_customers.address as cust_address,
+    wpcx_customers.city as cust_city,
+    wpcx_customers.state as cust_state,
+    wpcx_customers.zip as cust_zip,
+    wpcx_users.display_name as rep_display_name,
+    wpcx_users.email as rep_email
+  FROM wpcx_customers
+    INNER JOIN wpcx_users
+      ON wpcx_customers.location = wpcx_users.location
+    INNER JOIN wpcx_customers
+      ON wpcx_users.location = wpcx_customers.location
+  WHERE wpcx_customers.email=%s';
   $results = $wpdb->get_results($wpdb->prepare($query, $email));
-	return $results;
-	// wp_send_json_success($results);
+  return $results;
+  // wp_send_json_success($results);
 }
 
 add_action('wp_enqueue_scripts', 'plugin_scripts');
@@ -55,16 +57,16 @@ add_action('admin_enqueue_scripts', 'plugin_scripts');
 
 
 function plugin_scripts() {
-	// if(is_single()) {
-	// 	wp_enqueue_style('hooks', plugins_url('/index.css', __FILE__));
-	// }
+  // if(is_single()) {
+  // 	wp_enqueue_style('hooks', plugins_url('/index.css', __FILE__));
+  // }
 
-	wp_enqueue_script('hooks', plugins_url('/index.js', __FILE__));
-	// wp_enqueue_script('hooks', plugins_url('/index.js', __FILE__), array('jquery'), '1.0', true);
+  wp_enqueue_script('hooks', plugins_url('/index.js', __FILE__));
+  // wp_enqueue_script('hooks', plugins_url('/index.js', __FILE__), array('jquery'), '1.0', true);
 
-	// will emit an object accessible via: ajaxHooks.url
-	wp_localize_script('hooks', 'ajaxHooks', array(
-		'url' => admin_url('admin-ajax.php'),
-		'actions' => array('get_customer')
-	));
+  // will emit an object accessible via: ajaxHooks.url
+  wp_localize_script('hooks', 'ajaxHooks', array(
+    'url' => admin_url('admin-ajax.php'),
+    'actions' => array($actions)
+  ));
 }
