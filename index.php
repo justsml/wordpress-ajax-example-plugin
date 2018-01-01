59c536fc8ec2cf2a3dc3b152362d372fa0cf3ff0
Dan Levy <dan@danlevy.net>
2018-01-01 16:55:13 -0700

    cleanup/simplify code a bit

--- index.php
+++ index.php
@@ -10,27 +10,15 @@ License: MIT
 */
 $actions = array();
 
-function registerAjax($funcName, $requireLogin = true) {
-  $actions[] = $funcName;
-  if ($requireLogin) {
-    add_action('wp_ajax_nopriv_' . $funcName, '' . $funcName);
-  }
-  add_action('wp_ajax_' . $funcName, '' . $funcName);
-}
-
 /**
- * Setup get_customer as an ajax-callable function
+ * Example get_customer is an ajax-callable action/function
+ *
+ * Copy/Paste to add more actions (update_customer):
  */
 registerAjax('get_customer', true);
-function get_customer() {
+function get_customer($email) {
   global $wpdb;
   $email = $_POST['email'];
-  $results = search_customer($email);
-  wp_send_json_success($results);
-}
-
-function search_customer($email) {
-  global $wpdb;
   $query = 'SELECT wpcx_customers.salesrep,
     wpcx_customers.display_name as cust_display_name,
     wpcx_customers.email as cust_email,
@@ -47,26 +35,28 @@ function search_customer($email) {
     INNER JOIN wpcx_customers
       ON wpcx_users.location = wpcx_customers.location
   WHERE wpcx_customers.email=%s';
+
   $results = $wpdb->get_results($wpdb->prepare($query, $email));
-  return $results;
-  // wp_send_json_success($results);
+  wp_send_json_success($results);
 }
 
+/* Begin: WP Helpers */
 add_action('wp_enqueue_scripts', 'plugin_scripts');
 add_action('admin_enqueue_scripts', 'plugin_scripts');
-
-
+// include plugin script .css/.js
 function plugin_scripts() {
-  // if(is_single()) {
-  // 	wp_enqueue_style('hooks', plugins_url('/index.css', __FILE__));
-  // }
-
+  wp_enqueue_style('hooks', plugins_url('/index.css', __FILE__));
   wp_enqueue_script('hooks', plugins_url('/index.js', __FILE__));
-  // wp_enqueue_script('hooks', plugins_url('/index.js', __FILE__), array('jquery'), '1.0', true);
-
-  // will emit an object accessible via: ajaxHooks.url
+  // Emit an object accessible like so: ajaxHooks.url, ajaxHooks.actions
   wp_localize_script('hooks', 'ajaxHooks', array(
     'url' => admin_url('admin-ajax.php'),
     'actions' => array($actions)
   ));
+}
+function registerAjax($funcName, $requireLogin = true) {
+  $actions[] = $funcName;
+  if ($requireLogin) {
+    add_action('wp_ajax_nopriv_' . $funcName, '' . $funcName);
+  }
+  add_action('wp_ajax_' . $funcName, '' . $funcName);
 }
